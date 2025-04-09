63121137223b196905cfb24db16f336fbcfc3ca3
Hassnainabbasi <168479142+Hassnainabbasi@users.noreply.github.com>
2025-04-09 15:05:19 -0700

    Add

---
+++ udemy-work/src/App.tsx
@@ -0,0 +1,13 @@
+import './App.css'
+
+function App() {
+
+  return (
+   <>
+   <h1 className='text-center font-bold'>Hello World</h1>
+   <h1>Hello World</h1>
+   </>
+  )
+}
+
+export default App
